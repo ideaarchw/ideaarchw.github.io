9a2fc40f036325b7c4d8a2b44ef2f2c0e5f55459
ideaarchw <ideaarchw@gmail.com>
2026-02-10 18:57:18 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+
+function loadComponents() {
+    // This workaround loads the header and the footer, 
+    // and insert it into the proper divs (based on their IDs)
+    // to avoid duplication across multiple pages.
+    
+    header = `
+    <header>
+    <nav>
+        <a href="index.html">
+            <section class="hero-logo">
+                <img src="images/IDEA-ARCH png Vertical-01.png" alt="IDEA-Arch 2026 Logo" height="60px"  class="idea-logo hero-logo-svg">
+            </section>
+        </a>
+        <ul class="main__ul">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="dates.html">Important Dates</a></li>
+            <li><a href="Call_For_Paper.html">Call For Papers</a></li>
+            <li><a href="#">Committees</a>
+            <ul class="dropdown">
+                <li><a href="Organizing_Committee.html">Organizing Committee</a></li>
+                <li><a href="Keynote_Speaker.html">Keynote Speakers</a></li>
+            </ul>
+            </li>
+            <li><a href="Contact.html">Contact</a></li>
+            <li><a href="#" class="link-btn">Registration</a></li>
+        </ul>
+    </nav>
+    <div id='menu' class='box-icon'><i class='bx bx-menu'></i></div>
+    </header>
+    `
+
+    footer = `
+    <footer>
+        <p>© Copyright 2026. IDEA-ARCH 2025 - Designed by Muhammad Haroon</p>
+    </footer>
+    `
+
+    document.getElementById('header').innerHTML = header;
+    document.getElementById('footer').innerHTML = footer;
+
+   document.querySelector('#menu').addEventListener('click', () => {
+    const menu = document.querySelector('nav ul');
+    menu.classList.toggle('showmenu');
+    document.body.style.overflow = menu.classList.contains('showmenu') ? 'hidden' : '';
+});
+
+
+}
+
+window.onload = loadComponents;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const section = document.querySelector(".main-content-inner");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        section.classList.add("visible");
+        observer.disconnect();
+      }
+    });
+  }, { threshold: 0.3 });
+
+  observer.observe(section);
+});
+
